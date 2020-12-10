9d25ba0ac376123784f81e479ac1c4826a4219b6
CSWILIK <cswilik@gmail.com>
2020-12-09 21:54:08 -0500

    commit tree e886a4e8dea303e391b8c8fff55f15f516acdb59

---
+++ index.js
@@ -0,0 +1,40 @@
+
+// function wakeDog(dogName, dogBreed) {
+//     console.log(`Wake ${dogName} the ${dogBreed}`)
+//   }
+
+  function wakeDog(dogName, dogBreed) {
+    console.log(`Wake ${dogName} the ${dogBreed}`);
+  }
+
+//   function leashDog(dogName, dogBreed) {
+//     action = console.log(`Leash ${dogName} the ${dogBreed}`);
+//     return action
+//   }
+//   function walkToPark(dogName, dogBreed) {
+//     action = console.log(`Walk to the park with ${dogName} the ${dogBreed}`);
+//     return action
+//   }
+//   function throwFrisbee(dogName, dogBreed) {
+//     action = console.log(`Throw the frisbee for ${dogName} the ${dogBreed}`);
+//     return action
+//   }
+//   function walkHome(dogName, dogBreed) {
+//     action = console.log(`Walk home with ${dogName} the ${dogBreed}`);
+//     return action
+//   }
+//   function unleashDog(dogName, dogBreed) {
+//     action = console.log(`Unleash ${dogName} the ${dogBreed}`)
+//     return action
+//   }
+
+// const routine = [ wakeDog, leashDog, walkToPark, throwFrisbee, walkHome, unleashDog]
+
+// function exerciseDog(dogName, dogBreed) {
+//     i = 0 
+//     while (i < routine.length) { 
+//         console.log(i);
+//         i++ 
+//     }
+
+// }
